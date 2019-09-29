5bc3f109518692a8a81962d602bfd072cc6a37c7
hameda169 <169hamed@gmail.com>
2019-09-29 19:23:09 +0330

    socket changed to namespace class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_socketio import SocketIO, join_room, leave_room, emit, Namespace
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from random import shuffle
@@ -20,9 +20,86 @@ def match_cards(*c):
     return all(map(lambda i: len({c[0][i], c[1][i], c[2][i]}) in [1, len(c)], range(len(c[0]))))
 
 
+class MyGame(Namespace):
+    @staticmethod
+    def on_join(message):
+        room = message['room']
+        room = my_mongodb.db.rooms.find_one({'id': room}, {'started': 1, 'wins': 1, 'id': 1})
+        if not room or room['started']:
+            emit('error', dict(message='Room is not exists or is closed'))
+            return
+        wins = room['wins']
+        print(f'received message from {request.sid}: ' + str(message))
+        wins[f'{request.sid}'] = []
+        join_room(room['id'])
+        my_mongodb.db.rooms.update_one({'id': room['id']}, {'$set': {'wins': wins}})
+        emit('join_success', dict(data=dict(id=f'{request.sid}')))
+
+    @staticmethod
+    def on_start_room(data):
+        room = my_mongodb.db.rooms.find_one({'id': data['id']}, {'my_cards': 1, 'active_cards': 1, 'id': 1})
+        my_cards = room['my_cards']
+        active_cards = room['active_cards']
+        my_cards, x = my_cards[:-6], my_cards[-6:]
+        x = list(map(lambda y: f'000{br(y, base=3)}'[-4:], x))
+        active_cards = [*active_cards, *x]
+        my_mongodb.db.rooms.update_one({'id': data['id']},
+                                       {'$set': {'my_cards': my_cards, 'active_cards': active_cards, 'started': True}})
+        emit('init', dict(cards=x), room=room['id'])
+
+    @staticmethod
+    def on_challenge(data):
+        room = my_mongodb.db.rooms.find_one({'id': data['room'], f'wins.{request.sid}': {'$exists': True}})
+        restricted = room['restricted']
+        active_cards = room['active_cards']
+        wins = room['wins']
+        if request.sid == restricted:
+            # TODO
+            pass
+        elif not list(set(data['cards']) - set(active_cards)) and match_cards(*data['cards']):
+            restricted = ''
+            active_cards = list(set(active_cards) - set(data['cards']))
+            wins[f'{request.sid}'] = [*wins[f'{request.sid}'], *data['cards']]
+            res = dict(cards=data['cards'], sid=f'{request.sid}', wins=wins)
+            emit('challenge_success', res, room=room['id'])
+        else:
+            res = dict(cards=data['cards'], sid=f'{request.sid}')
+            emit('challenge_fail', res, room=room['id'])
+            restricted = request.sid
+        my_mongodb.db.rooms.update_one({'id': room['id']},
+                                       {'$set': {'restricted': restricted, 'active_cards': active_cards, 'wins': wins}})
+
+    @staticmethod
+    def on_deal(data):
+        room = my_mongodb.db.rooms.find_one({'id': data['room'], f'wins.{request.sid}': {'$exists': True}})
+        active_cards = room['active_cards']
+        my_cards = room['my_cards']
+        if len(active_cards) > 20:
+            res = dict(type='DEAL_FAIL', message="Table has more than 20 cards")
+            emit('deal_fail', res)
+            return
+        my_cards, x = my_cards[:-3], my_cards[-3:]
+        x = list(map(lambda y: f'000{br(y, base=3)}'[-4:], x))
+        active_cards = [*active_cards, *x]
+        my_mongodb.db.rooms.update_one({'id': room['id']},
+                                       {'$set': {'my_cards': my_cards, 'active_cards': active_cards}})
+        res = dict(cards=x)
+        emit('deal_success', res, room=room['id'], namespace='')
+
+    @staticmethod
+    def on_connect():
+        print(f'Client {request.sid} Connected')
+
+    @staticmethod
+    def on_disconnect():
+        rooms = my_mongodb.db.rooms.find({f'wins.{request.sid}': {'$exists': True}})
+        [leave_room(i['id']) for i in rooms]
+        print(f'Client {request.sid} disconnected')
+
+
 @app.route('/')
 def hello():
-    return 'Hello World'
+    return 'Hello World. Version 1.0'
 
 
 @app.route('/room', methods=['POST'])
@@ -40,97 +117,11 @@ def add_room():
 
 @app.route('/room/<room>')
 def get_room(room):
-    room = my_mongodb.db.rooms.find_one({'id': room})
-    if room:
-        del room['_id']
+    room = my_mongodb.db.rooms.find_one({'id': room}, {'_id': 0})
     return room if room else dict()
 
 
-@socketio.on('join')
-def my_connect(message):
-    room = message['room']
-    room = my_mongodb.db.rooms.find_one({'id': room})
-    if not room or room['started']:
-        emit('error', dict(message='Room is not exists or is closed'))
-        return
-    # user = dict(name=message['name'], room=room['id'], sid=f'{request.sid}')
-    # my_mongodb.db.users.insert_one(user)
-    wins = room['wins']
-    print(f'received message from {request.sid}: ' + str(message))
-    wins[f'{request.sid}'] = []
-    join_room(room['id'])
-    my_mongodb.db.rooms.update_one({'id': room['id']}, {'$set': {'wins': wins}})
-    emit('join_success', dict(data=dict(id=f'{request.sid}')))
-
-
-@socketio.on('startRoom')
-def start_room(data):
-    room = my_mongodb.db.rooms.find_one({'id': data['id']})
-    my_cards = room['my_cards']
-    active_cards = room['active_cards']
-    my_cards, x = my_cards[:-6], my_cards[-6:]
-    x = list(map(lambda y: f'000{br(y, base=3)}'[-4:], x))
-    active_cards = [*active_cards, *x]
-    my_mongodb.db.rooms.update_one({'id': data['id']},
-                                   {'$set': {'my_cards': my_cards, 'active_cards': active_cards, 'started': True}})
-    emit('init', dict(cards=x), room=room['id'])
-
-
-@socketio.on('challenge')
-def challenge(data):
-    room = my_mongodb.db.rooms.find_one({'id': data['room'], f'wins.{request.sid}': {'$exists': True}})
-    print(room)
-    restricted = room['restricted']
-    active_cards = room['active_cards']
-    wins = room['wins']
-    if request.sid == restricted:
-        # TODO
-        pass
-    elif not list(set(data['cards']) - set(active_cards)) and match_cards(*data['cards']):
-        restricted = ''
-        active_cards = list(set(active_cards) - set(data['cards']))
-        wins[f'{request.sid}'] = [*wins[f'{request.sid}'], *data['cards']]
-        res = dict(cards=data['cards'], sid=f'{request.sid}', wins=wins)
-        emit('challenge_success', res, room=room['id'])
-    else:
-        res = dict(cards=data['cards'], sid=f'{request.sid}')
-        emit('challenge_fail', res, room=room['id'])
-        restricted = request.sid
-    my_mongodb.db.rooms.update_one({'id': room['id']},
-                                   {'$set': {'restricted': restricted, 'active_cards': active_cards, 'wins': wins}})
-
-
-@socketio.on('deal')
-def deal(data):
-    room = my_mongodb.db.rooms.find_one({'id': data['room'], f'wins.{request.sid}': {'$exists': True}})
-    print(room)
-    active_cards = room['active_cards']
-    my_cards = room['my_cards']
-    if len(active_cards) > 20:
-        res = dict(type='DEAL_FAIL', message="Table has more than 20 cards")
-        emit('deal_fail', res)
-        return
-    my_cards, x = my_cards[:-3], my_cards[-3:]
-    x = list(map(lambda y: f'000{br(y, base=3)}'[-4:], x))
-    active_cards = [*active_cards, *x]
-    my_mongodb.db.rooms.update_one({'id': room['id']},
-                                   {'$set': {'my_cards': my_cards, 'active_cards': active_cards}})
-    res = dict(cards=x)
-    emit('deal_success', res, room=room['id'], namespace='')
-
-
-@socketio.on('connect')
-def test_connect():
-    print(f'Client {request.sid} Connected')
-
-
-@socketio.on('disconnect')
-def test_disconnect():
-    rooms = my_mongodb.db.rooms.find({f'wins.{request.sid}': {'$exists': True}})
-    print(rooms)
-    [leave_room(i['id']) for i in rooms]
-    print(f'Client {request.sid} disconnected')
-
+socketio.on_namespace(MyGame(''))
 
 if __name__ == '__main__':
     socketio.run(app)
